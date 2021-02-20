07eaf0191d080fd4d340f1055f274439a7b79ce0
informistica <informistica>
2021-02-20 18:53:26 +0100

    update login

--- esercizi_gianluca/login.js
+++ esercizi_gianluca/login.js
@@ -3,39 +3,39 @@
 const username="user";
 const password="pass";
 
-//  function login() {
-//   const userInserito=document.getElementById("txtUsername").value;
-//   const passInserita=document.getElementById("txtPassword").value;
-//   if(userInserito==username&&passInserita==password) {
-//     document.getElementsByTagName("form")[0].remove();
-//     document.getElementById("alert").remove();
-//     const div=document.createElement("div");
-//     const h1=document.createElement("h1");
-//     h1.innerText="Benvenuto "+userInserito;
-//     div.appendChild(h1);
-//     document.getElementById("contenitore").append(div);
-//   }else{
-//     const div=document.createElement("div");
-//     div.classList.add("alert");
-//     div.classList.add("alert-danger");
-//     div.role="alert";
-//     div.id="alert";
-//     div.innerText="Username o password errata";
-//     document.getElementById("contenitore").append(div);
-//   }
-// } 
+ function login() {
+  const userInserito=document.getElementById("txtUsername").value;
+  const passInserita=document.getElementById("txtPassword").value;
+  if(userInserito==username && passInserita==password) {
+    document.getElementsByTagName("form")[0].remove();
+    const div=document.createElement("div");
+    const h1=document.createElement("h1");
+    h1.innerText="Benvenuto "+userInserito;
+    div.appendChild(h1);
+    document.getElementById("contenitore").append(div);
+  }else{
+    const div=document.createElement("div");
+    div.classList.add("alert");
+    div.classList.add("alert-danger");
+    div.id="results";
+    div.innerText="Username o password errata";
+    document.getElementById("contenitore").append(div);
+  }
+} 
 
 /*global $*/ //Per eslint
-function login() {
-  const userInserito=$("#txtUsername").val();
-  const passInserita=$("#txtPassword").val();
-  if(userInserito==username && passInserita==password) {
-    $("form").remove();
-    $("#alert").remove();
-    const div=$("<div></div>").append($("<h1></h1>").text("Benvenuto "+username));
-    $("#contenitore").append(div);
-  }else{
-    const div=$("<div></div>").addClass("alert alert-danger").attr("role","alert").attr("id","alert").text("Username e/o password errati");
-    $("#contenitore").append(div);
-  }
-}
+
+//  function login() {
+//   const userInserito=$("#txtUsername").val();
+//   const passInserita=$("#txtPassword").val();
+//   if(userInserito==username && passInserita==password) {
+//    alert("Login effettuato");
+//    window.location.href="https://www.elexpo.net"
+//   }else{
+     
+//      const div=$("<div id='results'></div>").addClass("alert alert-danger").attr("id","results").text("Username e/o password errati");
+//     $("#contenitore").append(div);
+    
+//    }
+// }
+ 
